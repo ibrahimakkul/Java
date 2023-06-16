86852fab557f93c2272951661581b77abcb373df
İbrahim <102255701+ibrahimakkul@users.noreply.github.com>
2023-06-16 04:20:34 +0300

    Add files via upload

---
+++ PatikaStore/Objects/Notebook.java
@@ -0,0 +1,10 @@
+package Objects;
+
+import Product.Product;
+
+public class Notebook extends Product<Notebook> {
+
+    public Notebook(String brand, String description, double unitPrice, double discount, int stock, int memory, double size, int ram) {
+        super(brand, description, unitPrice, discount, stock,  memory, size, ram);
+    }
+}
---
+++ PatikaStore/Product/IProduct.java
@@ -0,0 +1,18 @@
+package Product;
+
+public interface IProduct {
+    public int getId();
+
+    public String getBrand();
+
+    public void setBrand(String name);
+
+    public double getUnitPrice();
+
+    public void setUnitPrice(double unitPrice);
+
+    public double getDiscount();
+
+    public void setDiscount(double discount);
+
+}
